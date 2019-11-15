b433e42df57c84c7f8902ccd7d077cc6deb9d6de
Martian <47183626+ariyanrad@users.noreply.github.com>
2019-11-15 21:38:50 +0330

    creating triangle with python string method

---
+++ triangle.py
@@ -0,0 +1,17 @@
+# making trinagle
+x= int(input('inter the hight of triangle: '))
+star = "*"
+for i in range(x):
+    print('{}'.format(star * i))
+
+
+# making homogine triangle
+
+for i in range(x):
+    print("{}".format((star*i).center(x+1)))
+
+# making right 90^ triangle
+
+for i in range(x):
+    print('{}'.format((star*i).rjust(x+1)))
+
